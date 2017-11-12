cc565a21552999a8030323c9aa49e597cb6e1144
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-12 20:19:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function BoardMember(name, homeState, training) {
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
+    veto() = function() {
+      return 'No, I must disagree';
+    }
+
+    approve() = function() {
+      return 'You can do that!';
+    }
+
+    doCharity() = function() {
+      return 'I like to help people.';
+    }
+}
